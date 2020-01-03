5f30bb25a292b221e133039f39d71e551fb1fdaa
Rahulnsharma45 <djrahul8130565174@gmail.com>
2020-01-04 02:19:34 +0530

    Adding game intialization function

--- app.js
+++ app.js
@@ -10,21 +10,9 @@ GAME RULES:
 */
  var scores, roundScore, activePlayer;
 
- scores = [0,0];
- roundScore = 0;
- activePlayer = 0;
+ init();
 
  dice = Math.floor(Math.random()*6) + 1;
-
- document.querySelector('#current-' + activePlayer).textContent = dice;
-
- document.querySelector('.dice').style.display = 'none';
-
- document.getElementById('score-0').textContent = '0';
- document.getElementById('score-1').textContent = '0';
- document.getElementById('current-0').textContent = '0';
- document.getElementById('current-1').textContent = '0';
-
 
  document.querySelector('.btn-roll').addEventListener('click', function() {
     
@@ -74,6 +62,27 @@ GAME RULES:
    document.querySelector('.player-1-panel').classList.toggle('active');
 
    document.querySelector('.dice').style.display = 'none';   
+ }
+
+ document.querySelector('.btn-new').addEventListener('click', init);
+
+ function init() {
+   scores = [0, 0];
+   activePlayer = 0;
+   roundScore = 0;
+
+   //document.querySelector('#current-' + activePlayer).textContent = dice;
+
+   document.querySelector('.dice').style.display = 'none';
+
+   document.getElementById('score-0').textContent = '0';
+   document.getElementById('score-1').textContent = '0';
+   document.getElementById('current-0').textContent = '0';
+   document.getElementById('current-1').textContent = '0';
+   document.getElementById('name-0').textContent = 'Player 1';
+   document.getElementById('name-1').textContent = 'Player 2';
+   document.querySelector('.player-0-panel').classList.remove('winner');
+   document.querySelector('.player-1-panel').classList.remove('winner');
  }
 
 
